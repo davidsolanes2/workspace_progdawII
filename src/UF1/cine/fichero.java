0d519897f287ac73d9a0687d280032516a46574f
david <davidsolanes2@gmail.com>
2016-10-05 16:39:43 +0200

    version 2.1 , incluyendo ficheros XML

--- src/UF1/cine/fichero.java
+++ src/UF1/cine/fichero.java
@@ -1,7 +1,27 @@
 package UF1.cine;
 
-/**
+/*
  * Created by dsg on 5/10/16.
  */
+
+import java.beans.XMLEncoder;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class fichero {
+
+    private String nombre;
+
+    public fichero(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void grabar (Object o) {
+        try {
+            XMLEncoder codificador = new XMLEncoder(new FileOutputStream(new File(nombre)));
+        } catch (FileNotFoundException ex) {
+            System.out.println("Error no se ha podido grabar: " + ex.getMessage());
+        }
+    }
 }
